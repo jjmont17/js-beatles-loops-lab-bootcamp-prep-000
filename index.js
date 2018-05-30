1d26a4591ede8b9b3c43c1ba6730f23d1e50d2b7
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-30 18:34:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  var allMusicians = [];
+  for( var i =0; i< musicians. length; i++)
+  {
+  allMusicians.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return allMusicians;
+}
+
+function johnLennonFacts()
+{
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  var newFacts = []
+  var i = 0
+  while ( i<facts.length)
+  {
+  newFacts.push(facts[i] + '!!!')
+}
+ return newFacts;
+}
